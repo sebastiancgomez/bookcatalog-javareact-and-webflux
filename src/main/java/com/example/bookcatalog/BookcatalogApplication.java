11f6d2d9ed4a4887e68bfd079a1e2c7b83c1a705
sebastiancgomez <sebastian.cgomez@gmail.com>
2026-03-05 19:20:54 -0500

    solution open-api compatibility

--- src/main/java/com/example/bookcatalog/BookcatalogApplication.java
+++ src/main/java/com/example/bookcatalog/BookcatalogApplication.java
@@ -1,13 +1,7 @@
 package com.example.bookcatalog;
 
-import com.example.bookcatalog.model.Book;
-import com.example.bookcatalog.repository.BookRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication
 public class BookcatalogApplication {
